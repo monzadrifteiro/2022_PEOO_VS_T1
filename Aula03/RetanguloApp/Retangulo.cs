69ebb529f7ee462e910365b744a84fa4a8b54288
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2022-10-21 17:52:37 -0300

    Upload da Aula03

---
+++ Aula03/RetanguloApp/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetanguloApp
+{
+    class Retangulo
+    {
+        private double b, h;
+        public void SetBase(double v)
+        {
+            if (v > 0) b = v;
+        }
+        public void SetAltura(double v)
+        {
+            if (v > 0) h = v;
+        }
+        public double GetBase() => b;
+        public double GetAltura() => h;
+        public double CalcArea() => b * h;
+        public double CalcDiagonal() => Math.Sqrt(b * b + h * h);
+    }
+}
