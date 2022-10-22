6de5a7fef03b2a9dc41d5dc6cca70b449d8bee1c
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2022-10-21 23:07:50 -0300

    Upload da Lista de Exercícios - POO - 04

---
+++ Lista de Exercícios - POO - 04/AbastecimentoApp/Abastecimento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbastecimentoApp
+{
+    internal class Abastecimento
+    {
+        private double valorLitro, valorPago, kmRodados;
+        public void SetValorLitro(double vl)
+        {
+            if (vl > 0) valorLitro = vl;
+        }
+        public void SetValorPago(double vp)
+        {
+            if (vp > 0) valorPago = vp;
+        }
+        public void SetKmRodados(double kr)
+        {
+            if (kr > 0) kmRodados = kr;
+        }
+        public double MediaKmLitro() => kmRodados / (valorPago / valorLitro);
+        public double MediaReaisKm() => valorPago / kmRodados;
+
+    }
+}
---
+++ Lista de Exercícios - POO - 04/AbastecimentoApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AbastecimentoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecimento a = new Abastecimento();
+            a.SetKmRodados(double.Parse(txtKM.Text));
+            a.SetValorPago(double.Parse(txtValor.Text));
+            a.SetValorLitro(double.Parse(txtValorLitro.Text));
+            txtKMLitro.Text = a.MediaKmLitro().ToString("0.00");
+            txtRSKM.Text = a.MediaReaisKm().ToString("0.00");
+        }
+    }
+}
