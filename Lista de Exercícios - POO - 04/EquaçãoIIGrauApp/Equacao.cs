a2d67b0aea98c0c640eb5812007a2a53d7079b9e
monzadrifteiro <nunes.lima@academico.ifrn.edu.br>
2022-10-24 16:05:25 -0300

    Upload do exercício EquaçãoIIGrau

---
+++ Lista de Exercícios - POO - 04/EquaçãoIIGrauApp/Equacao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquaçãoIIGrauApp
+{
+    class Equacao
+    {
+        private double a, b, c;
+        public void SetABC(double a, double b, double c)
+        {
+            if (a != 0) this.a = a;
+        }
+        public void GetABC(out double a, out double b, out double c)
+        {
+            a = this.a;
+            b = this.b;
+            c = this.c;
+        }
+        public bool RaizesReais()
+        {
+            if (Delta() < 0) return false;
+            return true;
+        }
+        public double Delta()
+        {
+            return Math.Sqrt(b) * -4 * a * c;
+        }
+        public bool X1(out double x)
+        {
+           x = (-b + Math.Sqrt(Delta())) / 2 * a;
+           return true;
+        }
+        public bool X2(out double x)
+        {
+            x = (-b - Math.Sqrt(Delta())) / 2 * a;
+            return true;
+        }
+    }
+}
