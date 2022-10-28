d6e745ff4c014a5061cbfb504924f4a2c8390994
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2022-10-28 16:48:10 -0300

    Upload das questões AgendaApp e RetanguloApp

---
+++ Lista de Exercícios - POO - 04/AgendaApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgendaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(txtNome.Text);
+            Contato lindo = new Contato(txtNome.Text, txtEmail.Text, txtTelefone.Text);
+            list.Items.Add(lindo);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedItem != null)
+                list.Items.Remove(list.SelectedItem);
+        }
+    }
+}
