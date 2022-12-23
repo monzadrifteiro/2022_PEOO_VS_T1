6e7e73733f97bc8ba5449c9ec7991bb6f19c576b
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2022-12-23 17:41:27 -0300

    Adicionei o projeto EscolaApp ao repositório.

---
+++ EscolaApp/Turma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaApp
+{
+    class Turma
+    {
+        public int Id { get; set; }
+        public int Curso { get; set; }
+        public int Descricao { get; set; }
+        public int AnoLetivo { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} - {Curso} - {Descricao} - {AnoLetivo}";
+        }
+
+    }
+}
--- Lista de Exercícios - POO - 04/BingoApp/obj/Release/net5.0-windows/MainWindow.g.cs
+++ Lista de Exercícios - POO - 04/BingoApp/obj/Release/net5.0-windows/MainWindow.g.cs
@@ -101,7 +101,7 @@ namespace BingoApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BingoApp_mwwlewy4_wpftmp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BingoApp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
